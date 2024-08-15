1ad0ec32c0f7a8e4e9277fb02e63ed77b2fcee9b
L7dri <ayoubeh98@gmail.com>
2024-08-15 13:28:01 +0100

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Select the database (provided as an argument when running the script)
+USE alx_book_store;
+
+-- Query to get the full description of the Books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'Books';
